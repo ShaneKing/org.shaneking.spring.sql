02cb641ca553fec22c3b0f72143cb14958e0ddfe
ShaneKing <github@azolla.org>
2020-05-15 23:23:06 +0800

    add performance testing project

--- src/test/java/sktest/spring/sql/SpringUnit.java
+++ src/test/java/sktest/spring/sql/SpringUnit.java
@@ -1,9 +1,12 @@
 package sktest.spring.sql;
 
-import org.shaneking.spring.test.SKSpringUnit;
+import org.junit.runner.RunWith;
+import org.shaneking.test.SKUnit;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringUnitApplication.class)
-public class SpringUnit extends SKSpringUnit {
+public class SpringUnit extends SKUnit {
 
 }
