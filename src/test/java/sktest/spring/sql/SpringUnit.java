c7279a57beaf99b36796a9f34f70cf2961b23a6f
ShaneKing <github@azolla.org>
2020-06-03 00:57:50 +0800

    0.3.3

--- src/test/java/sktest/spring/sql/SpringUnit.java
+++ src/test/java/sktest/spring/sql/SpringUnit.java
@@ -1,12 +1,9 @@
 package sktest.spring.sql;
 
-import org.junit.runner.RunWith;
-import org.shaneking.test.SKUnit;
+import org.shaneking.spring.test.SKSpringUnit;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringUnitApplication.class)
-public class SpringUnit extends SKUnit {
+public class SpringUnit extends SKSpringUnit {
 
 }
